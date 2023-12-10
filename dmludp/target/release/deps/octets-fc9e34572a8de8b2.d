d54c7f0e55aca178bcaa4dd8c7fbf68c2a344a83
liuxiaotiao <1041688750@qq.com>
2023-12-10 18:37:24 -0500

    try to fix data_write bug: test 4

--- dmludp/target/release/deps/octets-fc9e34572a8de8b2.d
+++
@@ -1,7 +0,0 @@
-/Users/qingzheguo/Desktop/pytorch_test/pytorch_test/gloo/dmludp/target/release/deps/octets-fc9e34572a8de8b2.rmeta: /Users/qingzheguo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/octets-0.2.0/src/lib.rs
-
-/Users/qingzheguo/Desktop/pytorch_test/pytorch_test/gloo/dmludp/target/release/deps/liboctets-fc9e34572a8de8b2.rlib: /Users/qingzheguo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/octets-0.2.0/src/lib.rs
-
-/Users/qingzheguo/Desktop/pytorch_test/pytorch_test/gloo/dmludp/target/release/deps/octets-fc9e34572a8de8b2.d: /Users/qingzheguo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/octets-0.2.0/src/lib.rs
-
-/Users/qingzheguo/.cargo/registry/src/index.crates.io-6f17d22bba15001f/octets-0.2.0/src/lib.rs:
