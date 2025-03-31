dc4ca976cf86eb818d9594654ac0cad4f20f184d
liuxiaotiao <1041688750@qq.com>
2025-03-31 00:47:13 -0400

    update

--- gloo/send_buf.h
+++ gloo/send_buf.h
@@ -345,7 +345,7 @@ namespace dmludp{
             }else{
                 rcq.push_back(in_offset);
             }
-            std::cout<<"acknowledege_and_drop:"<<in_offset<<", count_:"<<ack_count<<std::endl;
+            // std::cout<<"acknowledege_and_drop:"<<in_offset<<", count_:"<<ack_count<<std::endl;
             if (ack_count == bits_set.size()){
                 meta_status = MetaFlag::Complete;
             }
