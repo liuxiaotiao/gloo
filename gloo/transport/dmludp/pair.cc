1a4d0023b9fc0a967df714502b6f625154c11059
liuxiaotiao <1041688750@qq.com>
2025-03-31 19:19:57 -0400

    update

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -792,7 +792,7 @@ bool Pair::protocal2read(){
                 dmludp_conn_reset_rx_len(dmludp_connection);
                 break;
               }
-              std::cout<<"3 read check:"<<rx_.getOpcode();<<std::endl;
+              std::cout<<"3 read check:"<<rx_.getOpcode()<<std::endl;
               if (rbuf){
                 dmludp_connection->send_packet_complete();
                 stop = true;
