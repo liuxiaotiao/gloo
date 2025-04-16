5e10896e4f2d4583e0a496db703953e76e67b142
liuxiaotiao <1041688750@qq.com>
2025-04-16 10:30:54 -0400

    update

--- gloo/connection.h
+++ gloo/connection.h
@@ -2402,7 +2402,7 @@ public:
             }
             
         }
-        std::cout << "process_acknowledge process_acknowledge:" << std::endl;
+        std::cout << "process_acknowledge:" << max_acknowleged << std::endl;
         // for (auto i = sendbufferqueue.start(); i < sendbufferqueue.end(); i = (i + 1) % 256){
         while (true)
         {
