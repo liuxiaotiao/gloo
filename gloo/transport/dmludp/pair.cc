85eaab2ef7a19425bfafea7934b70e8f954922b5
liuxiaotiao <1041688750@qq.com>
2025-04-21 08:20:04 -0400

    update

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -883,7 +883,7 @@ bool Pair::protocal2send(){
       }
       const auto nbytes = prepareWrite(op, buf, iov.data(), ioc);
 
-      bool connection_written = dmludp_connection->get_data(iov.data(), ioc, opcode);
+      bool connection_written = dmludp_connection->get_da090ta(iov.data(), ioc, opcode);
       if (!connection_written){
         return false;
       }
