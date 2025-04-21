eb8d555dafe21aa4cb0b2f9b934eeb1184595f4e
liuxiaotiao <1041688750@qq.com>
2025-04-20 20:44:07 -0400

    update

--- gloo/send_buf.h
+++ gloo/send_buf.h
@@ -346,7 +346,7 @@ namespace dmludp{
         } 
 
         void ack_check(){
-            std::cout << "ack_count:" << ack_count << ", " << bits_set.size() << std::endl;
+            std::cout << "ack_count:" << ack_count << ", " << bits_set.size() << ", " << bits_set.count() << std::endl;
         }
 
 
