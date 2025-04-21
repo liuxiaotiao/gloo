1fee09d1bca28456bdbcc47da6451e904ba6ff8e
liuxiaotiao <1041688750@qq.com>
2025-04-21 13:21:47 -0400

    update

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -874,14 +874,14 @@ bool Pair::protocal2send(){
   }else if(dmludp_connection->sendbufferqueue.size() < tx_.size()){
     std::cout << "2 sendbufferqueue.size:" << dmludp_connection->sendbufferqueue.size() << ", tx_:" << tx_.size() << std::endl;    
     for (auto i = dmludp_connection->sendbufferqueue.size(); i < tx_.size(); i++){
-      std::cout<<"1 i:"<<i<<std::endl;
+      
       NonOwningPtr<UnboundBuffer> buf;
       std::array<struct iovec, 2> iov;
       int ioc;
       auto &op = tx_[i];
-      std::cout<<"2 i:"<<i<<std::endl;
+
       const auto opcode = op.getOpcode();
-      std::cout<<"3 i:"<<opcode<<std::endl;
+      std::cout<<"1 i:"<<opcode<<std::endl;
       if (opcode == Op::SEND_UNBOUND_BUFFER) {
         buf = NonOwningPtr<UnboundBuffer>(op.ubuf);
         if (!buf) {
@@ -889,7 +889,7 @@ bool Pair::protocal2send(){
         }
       }
       const auto nbytes = prepareWrite(op, buf, iov.data(), ioc);
-
+      std::cout<<"2 i:"<<i<<std::endl;
       bool connection_written = dmludp_connection->get_data(iov.data(), ioc, opcode);
       if (!connection_written){
         return false;
