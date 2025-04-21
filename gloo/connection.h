2cbf4d3effb02a67ef79489c606ec7e6157d53e6
liuxiaotiao <1041688750@qq.com>
2025-04-20 21:48:39 -0400

    update

--- gloo/connection.h
+++ gloo/connection.h
@@ -2312,9 +2312,9 @@ public:
         //     std::cout<<"[zerocheck] "<<(int)pkt_difference<<", pkt_num:"<<pkt_num <<", current_loop_min:"<<current_loop_min<<", pkt_offset:"<<pkt_offset<<", "<<receive_connection_difference<<std::endl;
         // }
 
-        if (pkt_difference >= receive_connection_difference && receive_connection_difference >= 20){
-            std::cout<<"[check] "<<(int)pkt_difference<<", pkt_num:"<<pkt_num << ", pkt_offset:"<<pkt_offset<<", "<<receive_connection_difference<<std::endl;
-        }
+        // if (pkt_difference >= receive_connection_difference && receive_connection_difference >= 20){
+        //     std::cout<<"[check] "<<(int)pkt_difference<<", pkt_num:"<<pkt_num << ", pkt_offset:"<<pkt_offset<<", "<<receive_connection_difference<<std::endl;
+        // }
         
         receive_available_map[index] = 1;
         if (pkt_difference >= receive_connection_difference){
