207269bdbc434e30579f79b36aad7e4b66233b69
liuxiaotiao <1041688750@qq.com>
2025-05-04 17:53:56 -0400

    update

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -794,7 +794,7 @@ bool Pair::protocal2read(){
         }
       }
     }
-    dmludp_connection->recvCQ.receive_log();
+    // dmludp_connection->recvCQ.receive_log();
     // std::cout<<"read complete"<<std::endl;
     {
       auto sendbufferqueue_start_index = dmludp_connection->sendbufferqueue.start();
@@ -825,9 +825,10 @@ bool Pair::protocal2read(){
     }
     
   }
+  dmludp_connection->recvCQ.receive_log();
   if (tx_.empty()) {
     device_->registerDescriptor(fd_, EPOLLIN, this);
-    // std::cout<<"registerDescriptor EPOLLIN"<<std::endl;
+    std::cout<<"registerDescriptor EPOLLIN"<<std::endl;
   }else{
     device_->registerDescriptor(fd_, EPOLLIN | EPOLLOUT, this);
     // std::cout << "tx_:" << tx_.size() << std::endl;
@@ -890,7 +891,7 @@ bool Pair::protocal2send(){
     auto start_time = std::chrono::steady_clock::now();
     auto packet_ = dmludp_connection->send_packet();
     auto i = packet_.first;
-    // std::cout << "[send_packet] " << packet_.first << ", " << packet_.second << ", "<< dmludp_connection->recovery.cwnd_available()<< std::endl;
+    std::cout << "[send_packet] " << packet_.first << ", " << packet_.second << ", "<< dmludp_connection->recovery.cwnd_available()<< std::endl;
     
     for ( ;i <= packet_.second; i++){
       auto retval = sendmsg(fd_, &dmludp_connection->send_message[i].message_body, 0);
@@ -910,17 +911,17 @@ bool Pair::protocal2send(){
       accumulated++;
     }
 
-    // struct sockaddr_in peer_addr;
-    // socklen_t addr_len = sizeof(peer_addr);
-    // if (getpeername(fd_, (struct sockaddr*)&peer_addr, &addr_len) < 0) {
-    //     perror("getpeername failed");
-    //     return 1;
-    // }
+    struct sockaddr_in peer_addr;
+    socklen_t addr_len = sizeof(peer_addr);
+    if (getpeername(fd_, (struct sockaddr*)&peer_addr, &addr_len) < 0) {
+        perror("getpeername failed");
+        return 1;
+    }
 
-    // char ip_str[INET_ADDRSTRLEN];
-    // inet_ntop(AF_INET, &peer_addr.sin_addr, ip_str, sizeof(ip_str));
+    char ip_str[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &peer_addr.sin_addr, ip_str, sizeof(ip_str));
 
-    // std::cout << "Send to: " << ip_str << ":" << ntohs(peer_addr.sin_port) << ", " << sent << std::endl;
+    std::cout << "Send to: " << ip_str << ":" << ntohs(peer_addr.sin_port) << ", " << sent << std::endl;
     if(sent == 0){
       device_->registerDescriptor(fd_, EPOLLIN, this);
 
