e2140dfc225fb266b953736fb3d78579629d8c91
liuxiaotiao <1041688750@qq.com>
2025-05-15 22:15:48 -0400

    update

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -811,12 +811,14 @@ bool Pair::protocal2read(){
                 // std::cout<<"1 read:"<<(int)dmludp_connection->receive_connection_difference<<std::endl;
                 dmludp_connection->update_receive_difference();
               }else{
+                std::cout<<"2 rx_set"<<std::endl;
                 dmludp_connection->rx_set(rnbytes, reinterpret_cast<uint8_t*>(riov.iov_base));
                 dmludp_connection->complete_check();
               }
               break;
             }
             // std::cout<<"rx_set:"<<(void*)(riov.iov_base)<<std::endl;
+            std::cout<<"1 rx_set"<<std::endl;
             dmludp_connection->rx_set(rnbytes, reinterpret_cast<uint8_t*>(riov.iov_base));
             if(dmludp_connection->send_packet_type == 0){
               dmludp_connection->send_packet_type = 5;
