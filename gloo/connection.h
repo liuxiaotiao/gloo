78a84a4574cc02f82956b836652b209f2eef25c8
liuxiaotiao <1041688750@qq.com>
2025-06-01 21:35:23 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -1806,6 +1806,7 @@ public:
 
 
         if (first_pn >= (max_acknowleged + 1)){
+            ip_print(peeraddr);
             std::cout<<"pkt_num:"<<pkt_num << ", " << first_pn << ", " << (max_acknowleged+1) <<", "<<tsInfo.size()<< std::endl;
             auto ackts = tsInfo.removeBeforeValue(first_pn);
             if (ackts.has_value()){
