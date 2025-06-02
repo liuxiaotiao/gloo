25c63709e7de4c790c4409dfb441200dc08d64f4
liuxiaotiao <1041688750@qq.com>
2025-06-02 09:25:17 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -2175,15 +2175,15 @@ public:
             recovery.on_packet_ack(total_send, receivets, std::chrono::duration_cast<std::chrono::seconds>(minrtt));
         }
 
-        // {
-        //     auto sendbufferqueue_start_index = sendbufferqueue.start();
-        //     for (auto idx = 0; idx < sendbufferqueue.get_count(); idx++){
-        //         int index = (sendbufferqueue_start_index + idx) % sendbufferqueue.get_capacity();
-        //         auto difference_ = sendbufferqueue.data_[index].get_difference();
-        //         std::cout << difference_ << " " ;
-        //         sendbufferqueue.data_[index].metabuf.ack_check();
-        //     }
-        // }
+        {
+            auto sendbufferqueue_start_index = sendbufferqueue.start();
+            for (auto idx = 0; idx < sendbufferqueue.get_count(); idx++){
+                int index = (sendbufferqueue_start_index + idx) % sendbufferqueue.get_capacity();
+                auto difference_ = sendbufferqueue.data_[index].get_difference();
+                std::cout << difference_ << " " ;
+                sendbufferqueue.data_[index].metabuf.ack_check();
+            }
+        }
     }
 
 
