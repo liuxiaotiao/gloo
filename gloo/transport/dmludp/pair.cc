a706151260b04d4ad4a79d803de0b9783721d9ec
liuxiaotiao <1041688750@qq.com>
2025-06-02 22:10:50 -0400

    debug

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -900,7 +900,7 @@ bool Pair::protocal2read(){
       }
     }
     // std::cout<<"speed:"<<std::chrono::duration_cast<std::chrono::nanoseconds>(endts-startts).count()/received<<" ns/packets"<<std::endl;
-
+    std::cout<<"protocal2read 1"<<std::endl;
     // auto flag4send = dmludp_connection->recv_slice2(received, receive_check);
     if (flag4send){
       // std::cout<<"sent_result:"<<sent_result<<std::endl;
@@ -1007,6 +1007,7 @@ bool Pair::protocal2read(){
     }
     // dmludp_connection->recvCQ.receive_log();
     // std::cout<<"read complete"<<std::endl;
+    std::cout<<"protocal2read 2"<<std::endl;
     {
       auto sendbufferqueue_start_index = dmludp_connection->sendbufferqueue.start();
       auto sendbufferqueue_count = dmludp_connection->sendbufferqueue.get_count();
@@ -1037,6 +1038,7 @@ bool Pair::protocal2read(){
     }
     
   }
+  std::cout<<"protocal2read 3"<<std::endl;
   dmludp_connection->recvCQ.receive_log(dmludp_connection->peeraddr);
   // std::cout<<"epollin:"<<tx_.size()<<std::endl;
   if (tx_.empty()) {
