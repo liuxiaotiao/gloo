ab0c19844d9bb12cd640169a5557308182e7e6af
liuxiaotiao <1041688750@qq.com>
2025-06-02 20:35:17 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -2356,7 +2356,7 @@ public:
                 }
 
                 auto pn = pkt_num_spaces.updatepktnum();
-                std::cout<<"prepareData:"<<pn<<", "<<out_off<<", "<<pkg_difference<<std::endl;
+                // std::cout<<"prepareData:"<<pn<<", "<<out_off<<", "<<pkg_difference<<std::endl;
        
                 send_message[sent].setMessageHeader(pn, out_off, pkg_difference, (Packet_num_len)out_len);
                 recovery.on_packet_sent(out_len);
