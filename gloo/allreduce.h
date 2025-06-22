31f25c8d0efdad6f2c50af252aef3214ea3c0fd6
liuxiaotiao <1041688750@qq.com>
2025-06-22 15:57:14 -0400

    debug

--- gloo/allreduce.h
+++ gloo/allreduce.h
@@ -190,6 +190,6 @@ class AllreduceOptions {
   friend void allreduce(const AllreduceOptions&, const std::vector<uint64_t> topkbitmap = {});
 };
 
-void allreduce(const AllreduceOptions& opts, const std::vector<uint64_t> topkbitmap = {})
+void allreduce(const AllreduceOptions& opts, const std::vector<uint64_t> topkbitmap = {});
 
 } // namespace gloo
