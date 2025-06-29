185db7a78ee8143c80233ed64893b8ab39804727
liuxiaotiao <1041688750@qq.com>
2025-06-29 18:05:52 -0400

    debug

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -725,6 +725,8 @@ bool Pair::protocal2read(){
             
             if (i == 1){
               if (rnbytes == 0){
+                ip_print(dmludp_connection->peeraddr);
+                std::cout<<dmludp_connection->receive_connection_difference<<", ";
                 readComplete(rbuf);
 
                 dmludp_connection->update_receive_difference();
@@ -771,6 +773,8 @@ bool Pair::protocal2read(){
 
             const auto rnbytes = prepareRead(rx_, rbuf, riov);
             if (rnbytes == 0){
+              ip_print(dmludp_connection->peeraddr);
+              std::cout<<dmludp_connection->receive_connection_difference<<", ";
               readComplete(rbuf);
 
               dmludp_connection->update_receive_difference();
