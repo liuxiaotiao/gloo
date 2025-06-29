879bb62ecd8509e67a8149a0118d62476bb471c3
liuxiaotiao <1041688750@qq.com>
2025-06-29 18:55:20 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -1997,7 +1997,7 @@ public:
         // }else{
         //     recovery.on_packet_ack(total_send, receivets, std::chrono::duration_cast<std::chrono::seconds>(minrtt));
         // }
-        std::cout<<recovery.cwnd_available<<", "<<low_recovery.cwnd_available<<std::endl;
+        std::cout<<recovery.cwnd_available()<<", "<<low_recovery.cwnd_available()<<std::endl;
     }
 
     /*Update receive difference to process next block data*/
