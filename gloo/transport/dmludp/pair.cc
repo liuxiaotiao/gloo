ed7a41df8d0b34de0db03692523b8f0b59390b6f
liuxiaotiao <1041688750@qq.com>
2025-06-29 21:56:00 -0400

    debug

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -801,7 +801,7 @@ bool Pair::protocal2read(){
         }
       }
     }
-    // dmludp_connection->recvCQ.receive_log();
+    dmludp_connection->recvCQ.receive_log();
     // std::cout<<"read complete"<<std::endl;
     {
       auto sendbufferqueue_start_index = dmludp_connection->sendbufferqueue.start();
