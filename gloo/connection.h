75e9c9aab43bfce61bf2d28547068b1b42a0f41c
liuxiaotiao <1041688750@qq.com>
2025-06-29 21:22:25 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -656,11 +656,11 @@ class metarecebuf{
             }
             status_ = 4;
 
-            if (important_packet_count > expected_important_packets) {
-                std::cout<<"[Error] important_packet_count:"<<important_packet_count<<", expected_important_packets:" << 
-                (expected_important_packets.has_value() ? std::to_string(expected_important_packets.value()) : "<nullopt>")<< std::endl;
-                _Exit(0);
-            }
+            // if (important_packet_count > expected_important_packets) {
+            //     std::cout<<"[Error] important_packet_count:"<<important_packet_count<<", expected_important_packets:" << 
+            //     (expected_important_packets.has_value() ? std::to_string(expected_important_packets.value()) : "<nullopt>")<< std::endl;
+            //     _Exit(0);
+            // }
         
             bool complete_ = unimportant_packets_status.has_value() && expected_important_packets == important_packet_count;
 
