9016321037ac0a17875cdb4249ead0deaabf5aff
liuxiaotiao <1041688750@qq.com>
2025-06-30 10:38:57 -0400

    debug

--- gloo/transport/dmludp/pair.cc
+++ gloo/transport/dmludp/pair.cc
@@ -330,8 +330,8 @@ bool Pair::write(Op& op) {
 
 void Pair::writeComplete(const Op &op, NonOwningPtr<UnboundBuffer> &buf,
                          const Op::Opcode &opcode) const {
-  ip_print(dmludp_connection->peeraddr);
-	std::cout<<"writeComplete:"<<opcode<<", "<<tx_.size()<<std::endl;
+  // ip_print(dmludp_connection->peeraddr);
+	// std::cout<<"writeComplete:"<<opcode<<", "<<tx_.size()<<std::endl;
   switch (opcode) {
     case Op::SEND_BUFFER:
       op.buf->handleSendCompletion();
@@ -517,7 +517,7 @@ bool Pair::read() {
 
 void Pair::readComplete(NonOwningPtr<UnboundBuffer> &buf) {
   const auto opcode = this->rx_.getOpcode();
-  std::cout<<"readComplete:"<<opcode<<std::endl;
+  // std::cout<<"readComplete:"<<opcode<<std::endl;
   switch (opcode) {
     case Op::SEND_BUFFER:
       // Done sending data to pinned buffer; trigger completion.
@@ -725,8 +725,8 @@ bool Pair::protocal2read(){
             
             if (i == 1){
               if (rnbytes == 0){
-                std::cout<<dmludp_connection->receive_connection_difference<<", ";
-                ip_print(dmludp_connection->peeraddr);
+                // std::cout<<dmludp_connection->receive_connection_difference<<", ";
+                // ip_print(dmludp_connection->peeraddr);
                 readComplete(rbuf);
 
                 dmludp_connection->update_receive_difference();
@@ -773,8 +773,8 @@ bool Pair::protocal2read(){
 
             const auto rnbytes = prepareRead(rx_, rbuf, riov);
             if (rnbytes == 0){
-              std::cout<<dmludp_connection->receive_connection_difference<<", ";
-              ip_print(dmludp_connection->peeraddr);       
+              // std::cout<<dmludp_connection->receive_connection_difference<<", ";
+              // ip_print(dmludp_connection->peeraddr);       
               readComplete(rbuf);
 
               dmludp_connection->update_receive_difference();
@@ -819,7 +819,7 @@ bool Pair::protocal2read(){
           }
           op.nwritten = dmludp_connection->sendbufferqueue.frontsent();
           if (op.nwritten == op.preamble.nbytes){
-            std::cout<<dmludp_connection->sendbufferqueue.at(0).get_difference()<<", ";
+            // std::cout<<dmludp_connection->sendbufferqueue.at(0).get_difference()<<", ";
             writeComplete(op, sbuf, opcode);
             tx_.pop_front();
             dmludp_connection->sendbufferqueue.pop_front();
