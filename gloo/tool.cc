f1c511012952827f4dc93c8ac24444ec98be19d9
liuxiaotiao <1041688750@qq.com>
2025-06-29 20:41:09 -0400

    debug

--- gloo/tool.cc
+++ gloo/tool.cc
@@ -303,7 +303,7 @@ namespace dmludp {
         int64_t pos = find_next_bit_avx2(bits_, num_bits_,
                                         next_one_index_ - start_bit_,
                                         end_bit_ - start_bit_, true);
-        std::cout<<"next_one_avx512:"<<start_bit_<<", "<<start_bit_<<", "<<next_one_index_<<std::endl;
+        std::cout<<"next_one_avx512:"<<start_bit_<<", "<<start_bit_<<", "<<next_one_index_<<", "<<pos<<std::endl;
         if (pos != -1) {
             pos += start_bit_;
             next_one_index_ = pos + 1;
