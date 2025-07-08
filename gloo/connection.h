f81a9fad48ebd74add5684c8d2f3422db6a5b8e0
liuxiaotiao <1041688750@qq.com>
2025-07-07 22:09:37 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -1985,6 +1985,18 @@ public:
             low_recovery.on_packet_ack(total_unimportant, receivets, std::chrono::duration_cast<std::chrono::seconds>(minrtt));
         }
 
+        ip_print(peeraddr);
+        std::cout<<recovery.cwnd_available()<<", "<<low_recovery.cwnd_available()<<", "<<total_important<<", "<<total_unimportant<<std::endl;
+        
+        std::cout<<"send condition:" <<std::endl;
+        auto sendbufferqueue_start_index = sendbufferqueue.start();
+        for (auto idx = 0; idx < sendbufferqueue.get_count(); idx++){
+            int index = (sendbufferqueue_start_index + idx) % sendbufferqueue.get_capacity();
+            auto difference_ = sendbufferqueue.data_[index].get_difference();
+            std::cout << difference_ << " " ;
+            sendbufferqueue.data_[index].metabuf.ack_check();
+        }
+
     }
 
     /*Update receive difference to process next block data*/
