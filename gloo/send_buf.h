2ac1976ce5f32c162e3d749986a92f03c538846b
liuxiaotiao <1041688750@qq.com>
2025-07-12 07:25:18 -0400

    debug

--- gloo/send_buf.h
+++ gloo/send_buf.h
@@ -701,7 +701,7 @@ namespace dmludp{
         }
 
         void ack_check(){
-            std::cout << "ack_count:" << ack_count_important << ", packet_count_important:"<<packet_count_important<< ", bits_set.size:" << bits_set.size() << ",\nbits_set.count" << bits_set.count() <<", rcq_important.size:"<< rcq_important.size() <<
+            std::cout << "ack_count:" << ack_count_important << ", packet_count_important:"<<packet_count_important<< ", bits_set.size:" << bits_set.size() << ", bits_set.count" << bits_set.count() <<", rcq_important.size:"<< rcq_important.size() <<
             ", meta_status_unimportant:" << static_cast<uint32_t>(meta_status_unimportant) << ",\nmeta_status_important:"<< static_cast<uint32_t>(meta_status_important) << std::endl;
             // ", lastpacketOffset_important:" << lastpacketOffset_important << ", lastpacketOffset_unimportant:" << lastpacketOffset_unimportant << ",\ninitlosscount_important:" << initlosscount_important << ", len:" << meta_ptr_len + meta_ptr2_len <<
             // ", " << meta_sent << std::endl;
