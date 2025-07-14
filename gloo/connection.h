b5066f45da4e2554e91749678eb0ad439e73cae5
liuxiaotiao <1041688750@qq.com>
2025-07-13 21:14:53 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -2282,7 +2282,8 @@ public:
 
     /*Use to clear send parameter*/
     void send_packet_complete(size_t err_ = 0, size_t sent = 0, const std::chrono::high_resolution_clock::time_point& start_ts = std::chrono::high_resolution_clock::time_point{}){
-        std::cout<<"err_:"<<err_<<", sent:"<<sent<<", start_index:"<<start_index<<", end_index:"<<end_index<<std::endl;
+        std::cout<<"err_:"<<err_<<", sent:"<<sent<<", start_index:"<<start_index<<", end_index:"<<end_index<<", "<<send_message[packet_.first].message_header.get_pkt_num()
+        <<", " <<send_message[packet_.second].message_header.get_pkt_num() <<std::endl;
         if(send_packet_type == 0){
             return;
         }
