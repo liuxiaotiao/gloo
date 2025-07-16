0a1261d00def570457e2c662038c3aaa487e0af1
liuxiaotiao <1041688750@qq.com>
2025-07-15 23:01:12 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -2310,7 +2310,8 @@ public:
 
     /*Use to clear send parameter*/
     void send_packet_complete(size_t err_ = 0, size_t sent = 0, std::chrono::high_resolution_clock::time_point start_ts = std::chrono::high_resolution_clock::time_point{}){
-        if(send_packet_type == 0){
+        std::cout << "Debug: send_packet_complete, err_:" << err_ << ", sent:" << sent << ", start_index:" << start_index << std::endl;
+        if(send_packet_type == 0){ 
             return;
         }
         if (send_packet_type == Type::Application){
