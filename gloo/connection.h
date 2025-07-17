37b3cc7420af7abf3cf74e0b0f0d1f86ac66cc55
liuxiaotiao <1041688750@qq.com>
2025-07-17 11:40:21 -0400

    debug

--- gloo/connection.h
+++ gloo/connection.h
@@ -232,7 +232,7 @@ class MessageFIFO{
                     cmsg->cmsg_level = SOL_UDP;
                     cmsg->cmsg_type = UDP_SEGMENT;
                     cmsg->cmsg_len = CMSG_LEN(sizeof(uint16_t));
-                    *((uint16_t*)CMSG_DATA(cmsg)) = GSO_SIZE;
+                    *((uint16_t*)CMSG_DATA(cmsg)) = MAX_SEND_UDP_PAYLOAD_SIZE + sizeof(Header);
 
                     iovecs[i * 3].iov_base = static_cast<void*>(&message_header[i]);
                     iovecs[i * 3].iov_len = sizeof(Header);
