f21b6d13d98815473657744787defd72b15c0c7a
liuxiaotiao <1041688750@qq.com>
2025-07-17 21:17:54 -0400

    debug

--- gloo/tool.h
+++ gloo/tool.h
@@ -18,6 +18,8 @@ namespace dmludp {
     // The default max_datagram_size used in congestion control.
     inline constexpr size_t MAX_SEND_UDP_PAYLOAD_SIZE = 8640;
 
+    inline constexpr size_t BATCH_SIZE = 7;
+
     inline constexpr size_t MAX_ACK_UDP_PAYLOAD_SIZE = 1400;
 
     inline constexpr size_t RX_CONST = 8192;
